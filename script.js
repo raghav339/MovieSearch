cbd12e1cb8e7d9c186511ac15c686353ee8255ea
raghav <raghavagarwal748@gmail.com>
2026-04-01 23:42:45 +0530

    made the moreInfo button responsive

--- script.js
+++ script.js
@@ -67,14 +67,19 @@ async function addMovie()
         container.className=`container container${index}`;
         // Create Image Div
         let imgDiv = document.createElement("div");
-        imgDiv.className = "pic";
+        imgDiv.className = `pic pic${index}`;
         imgDiv.style.backgroundImage = `url('${movie.Poster}')`;
             
-            // Create Details Div
+        // Create Details Div
         let detailDiv = document.createElement("div");
-        detailDiv.className = "details";
+        detailDiv.className = `details detail${index}`;
 
-            // Create and nest the paragraphs inside detailDiv
+        //create button div to store buttons
+        let btnDiv = document.createElement("div");
+        btnDiv.className = `btn${index}`;
+
+
+        // Create and nest the paragraphs inside detailDiv
         let p1 = document.createElement("p");
         p1.innerHTML = `<strong>${movie.Title}</strong>`;
             
@@ -86,9 +91,9 @@ async function addMovie()
 
         let btnMore=document.createElement("button");
         btnMore.innerText="Click to view more";
-        btnMore.className="btnStyle";
+        btnMore.className=`btnStyle moreBtn${index}`;
         btnMore.addEventListener("click",()=>{
-            console.log("added!");
+            moreInfo(movie.imdbID, index);
         });
 
         let btnFav=document.createElement("button");
@@ -98,10 +103,49 @@ async function addMovie()
             console.log("added!");
         });
 
-            // Structure: Put Ps inside DetailDiv, then put everything in Container
-        detailDiv.append(p1,p2,p3,btnMore,btnFav);
-        container.append(imgDiv, detailDiv);
+        // Structure: Put Ps inside DetailDiv, then put everything in Container
+        btnDiv.append(btnMore,btnFav);
+        detailDiv.append(p1,p2,p3,btnDiv);
+        container.append(imgDiv,detailDiv);
         document.querySelector(".bigContainer").append(container);
         })
         console.log(data);
+}
+
+async function moreInfo(Id,index)
+{
+    //disable button now 
+    document.querySelector(`.moreBtn${index}`).disabled=true;
+
+    const res=await fetch(`https://www.omdbapi.com/?i=${Id}&apikey=${API_KEY}`);
+    const data =await res.json();
+    // changing image size
+    const pic=document.querySelector(`.pic${index}`);
+    pic.style.height="350px" ;
+    pic.style.width="300px" ;
+    
+    // selecting details div
+    let detailDiv = document.querySelector(`.detail${index}`);
+
+    //selecting button div
+    let btn= document.querySelector(`.btn${index}`);
+
+    // Create and nest the paragraphs inside detailDiv
+    
+    let p2 = document.createElement("p");
+    p2.innerHTML = `<b>IMDb Rating</b>: ${data.imdbRating}`;
+    
+    let p4 = document.createElement("p");
+    p4.innerHTML = `<b>Plot</b>: ${data.Plot}`;
+
+    let p3 = document.createElement("p");
+    p3.innerHTML = `<b>Actors</b>: ${data.Actors}`;
+
+
+    // Structure: Put Ps inside DetailDiv, detailDiv already in container toh kuch karne ka req ni
+    detailDiv.insertBefore(p2, btn);
+    detailDiv.insertBefore(p3, btn);
+    detailDiv.insertBefore(p4, btn);
+
+    console.log(data);
 }
